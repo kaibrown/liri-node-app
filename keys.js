a042221ded0b15337ce4467fc518112fa9507d87
kaibrown <kaisey.brown@gmail.com>
2017-11-07 23:27:03 -0500

    creating keys.js file

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '4gB1ixseA2npLwlEha1gQh8pq',
+  consumer_secret: 'qGpwLokpUJp4vr53apkSorJHWVFeEiUwN9UBI2kG3cSu1Z9VNz',
+  access_token_key: '892847042589933569-A4QQ1pv1aOmv3BIMg7aLCcnCGIj65RX',
+  access_token_secret: 'mcwJzFAL8iDkJVBHPfbbk74VnaABroykqDWf3OC1OX29Z',
+};
